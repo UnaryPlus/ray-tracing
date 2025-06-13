241a0c52757fc21192a8bb2b69421b535c70dd67
UnaryPlus <ombspring@gmail.com>
2025-06-13 15:02:43 -0400

    part 3

--- main.cc
+++ main.cc
@@ -1,23 +1,17 @@
-// g++ create_ppm.cc -o create_ppm && ./create_ppm > image.ppm
-
-#include <cstdio>
+#include "color.h"
+#include <iostream>
 
 int main() {
     int image_width = 256;
     int image_height = 256;
 
-    printf("P3\n%d %d\n255\n", image_width, image_height);
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     for(int j = 0; j < image_height; j++) {
         for(int i = 0; i < image_width; i++) {
             double r = double(i) / (image_width - 1); 
             double g = double(j) / (image_height - 1);
             double b = 0.0;
-
-            int ir = int(r * 255.999);
-            int ig = int(g * 255.999);
-            int ib = int(b * 255.999);
-
-            printf("%d %d %d\n", ir, ig, ib);
+            write_color(std::cout, color(r, g, b));
         }
     }
 }

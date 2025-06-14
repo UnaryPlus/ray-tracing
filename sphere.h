42a152c66ef6342d2c64ee99c0e28c73f7c53570
UnaryPlus <ombspring@gmail.com>
2025-06-13 21:39:42 -0400

    part6

---
+++ sphere.h
@@ -0,0 +1,45 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "interval.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+  private:
+    point3 center;
+    double radius;
+
+  public:
+    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
+
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+        vec3 oc = center - r.origin();
+        double a = r.direction().length_squared();
+        double h = dot(r.direction(), oc);
+        double c = oc.length_squared() - radius*radius;
+
+        double discriminant = h*h - a*c;
+        if (discriminant < 0)
+            return false;
+
+        double sqrtd = std::sqrt(discriminant);
+
+        // Find the nearest root that lies in the acceptable range.
+        double root = (h - sqrtd) / a;
+        if (!ray_t.surrounds(root)) {
+            root = (h + sqrtd) / a;
+            if (!ray_t.surrounds(root))
+                return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+
+        return true;
+    }
+};
+
+#endif
